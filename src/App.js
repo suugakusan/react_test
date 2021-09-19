e3cef54d240784e3f7ba75f5848077d9937e085f
suugakusan <87798070+suugakusan@users.noreply.github.com>
2021-09-19 15:01:21 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Colorfull from "./components/Colorfull";
 
 const App = () => {
+  const onclickButton = () => alert();
+
   return (
     <>
-      <h1>こんにちは/h1>
-      <p>お元気ですか？</p>
+      <h1>こんにちは</h1>
+      <Colorfull color="blue">お元気ですか？</Colorfull>
+      <Colorfull color="pink">元気です！</Colorfull>
+      <button onClick={onclickButton}>ボタン</button>
     </>
   );
 };
