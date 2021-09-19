ce36fd54200ef6c38d4888f3b13148d3be75efa5
suugakusan <87798070+suugakusan@users.noreply.github.com>
2021-09-19 15:17:59 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Colorfull from "./components/Colorfull";
 
 const App = () => {
-  const onclickButton = () => alert();
-
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>こんにちは</h1>
       <Colorfull color="blue">お元気ですか？</Colorfull>
       <Colorfull color="pink">元気です！</Colorfull>
-      <button onClick={onclickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
